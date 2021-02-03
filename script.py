0c7eaa0a8215016513812a19290a4ea23b2167be
cirrusm <cirrus1994@gmail.com>
2021-02-02 20:52:07 -0800

    hi

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
-## SPOKEO
+## SPOKEO URL
 
 ## INPUTS
 ## URL TO PROFILE THAT NEEDS TO BE DELETED + EMAIL ADRESS TO SEND CONFIRMATION LINK TO
-
+opt_out = 'https://www.spokeo.com/optout'
+profile = input('Please Enter the link to the Spokeo Profile you would like to remove')
+email_adress = input('Please enter your email')
 ## OUTPUTS
+
+
+def remove_profile(email, url):
+## OPEN SPOKEO URL
 ## TYPE PROFILE URL INTO URL INPUT FIELD
 ## TYPE EMAIL ADRESS URL INTO EMAIL ADRESS FIELD
 
-## MOVE MOUS TO IM NOT A ROBOT CHECKBOX
+## MOVE MOUSE TO "IM NOT A ROBOT CHECKBOX"
 ## CLICK IM NOT A ROBOT CHECKBOX
 
 ## CLICK REMOVE THIS LISTING
