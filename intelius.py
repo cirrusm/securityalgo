4beb32e6253be227ffbb3541bebca44377ed8ef7
cirrusm <cirrus1994@gmail.com>
2021-02-03 22:31:43 -0800

    move to third page

--- intelius.py
+++ intelius.py
@@ -8,13 +8,11 @@ web.get('https://www.intelius.com/opt-out/')
 time.sleep(1)
 no_button = web.find_element_by_xpath('//*[@id="button-container"]/a[2]')
 no_button.click()
-## FILL OUT FIRST NAME LAST NAME STATE EMAIL ADRESS
+## FILL OUT FIRST NAME LAST NAME STATE EMAIL ADRESS CLICK CONTINUE
 time.sleep(1)
 first_name_field = web.find_element_by_xpath('//*[@id="first-name"]/div/input')
 last_name_field = web.find_element_by_xpath('//*[@id="last-name"]/div/input')
 email_field = web.find_element_by_xpath('//*[@id="email"]/div/input')
-
-
 
 first_name_field.send_keys('Cirrus')
 last_name_field.send_keys('Mokhtari')
@@ -22,4 +20,8 @@ email_field.send_keys('Cirrus1994@gmail.com')
 time.sleep(1)
 
 web.find_element_by_xpath("//select[@name='state']/option[text()='California']").click()
+
+time.sleep(1)
+web.find_element_by_xpath('//*[@id="submit"]/div/button').click()
 ## CLICK FIRST REMOVE RECORD
+time.sleep(1)
