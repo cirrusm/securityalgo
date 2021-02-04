7f8ac5d8c1b4837929953f5cfcc0d573e4ca48fc
cirrusm <cirrus1994@gmail.com>
2021-02-03 22:28:46 -0800

    second intelius page works

--- intelius.py
+++ intelius.py
@@ -2,8 +2,24 @@ from selenium import webdriver
 import time
 ## OPEN INTELIUS PAGE
 
+web = webdriver.Chrome()
+web.get('https://www.intelius.com/opt-out/')
 ## CLICK NO
-
+time.sleep(1)
+no_button = web.find_element_by_xpath('//*[@id="button-container"]/a[2]')
+no_button.click()
 ## FILL OUT FIRST NAME LAST NAME STATE EMAIL ADRESS
+time.sleep(1)
+first_name_field = web.find_element_by_xpath('//*[@id="first-name"]/div/input')
+last_name_field = web.find_element_by_xpath('//*[@id="last-name"]/div/input')
+email_field = web.find_element_by_xpath('//*[@id="email"]/div/input')
 
+
+
+first_name_field.send_keys('Cirrus')
+last_name_field.send_keys('Mokhtari')
+email_field.send_keys('Cirrus1994@gmail.com')
+time.sleep(1)
+
+web.find_element_by_xpath("//select[@name='state']/option[text()='California']").click()
 ## CLICK FIRST REMOVE RECORD
