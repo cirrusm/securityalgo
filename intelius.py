c3de82ee743912619f221e4c4a2c68864138959d
cirrusm <cirrus1994@gmail.com>
2021-02-04 19:55:29 -0800

    added variables

--- intelius.py
+++ intelius.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
 import time
+
+option = webdriver.ChromeOptions()
+firstname = input('Please enter your first name: ')
+lastname = input('Please enter your last name: ')
+email = input('Please enter your email: ')
+option.add_argument('--disable-blink-features=AutomationControlled')
 ## OPEN INTELIUS PAGE
 
-web = webdriver.Chrome()
+web = webdriver.Chrome(options=option)
 web.get('https://www.intelius.com/opt-out/')
 ## CLICK NO
 time.sleep(1)
 no_button = web.find_element_by_xpath('//*[@id="button-container"]/a[2]')
 no_button.click()
-## FILL OUT FIRST NAME LAST NAME STATE EMAIL ADRESS CLICK CONTINUE
+# FILL OUT FIRST NAME LAST NAME STATE EMAIL ADRESS CLICK CONTINUE
 time.sleep(1)
 first_name_field = web.find_element_by_xpath('//*[@id="first-name"]/div/input')
 last_name_field = web.find_element_by_xpath('//*[@id="last-name"]/div/input')
 email_field = web.find_element_by_xpath('//*[@id="email"]/div/input')
 
-first_name_field.send_keys('Cirrus')
-last_name_field.send_keys('Mokhtari')
+first_name_field.send_keys(firstname)
+last_name_field.send_keys(lastname)
 email_field.send_keys('Cirrus1994@gmail.com')
 time.sleep(1)
 
 web.find_element_by_xpath("//select[@name='state']/option[text()='California']").click()
 
-time.sleep(1)
 web.find_element_by_xpath('//*[@id="submit"]/div/button').click()
-## CLICK FIRST REMOVE RECORD
-time.sleep(1)
+# CLICK FIRST REMOVE RECORD
+
+web.find_element_by_xpath('//*[@id="open-result-1"]').click()
