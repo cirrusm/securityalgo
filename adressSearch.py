946373ce42bc104be2634c3107766d43c4f5bc8f
cirrusm <cirrus1994@gmail.com>
2021-02-06 18:32:26 -0800

    adresssearch

---
+++ adressSearch.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+
+## INPUT VARIABLES
+firstname = 'Cirrus'
+lastname = 'Mokhtari'
+address = '4501 surrey drive'
+city = 'Corona del mar'
+zip_code = '92625'
+email = 'Cirrus1994@gmail.com'
