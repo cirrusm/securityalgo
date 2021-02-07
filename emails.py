549232be45f1e90d2544345974716e033c5321cc
cirrusm <cirrus1994@gmail.com>
2021-02-07 12:12:49 -0800

    made send mail a function

--- emails.py
+++ emails.py
@@ -3,17 +3,19 @@ import smtplib
 
 ## should prob make this a function 
 ## pass it an array of objects containing users info, + a profile link for each object
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+def sendmail(name, url):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
 
-    smtp.login('cirrus1994@gmail.com', 'vegasfat1')
+        smtp.login('cirrus1994@gmail.com', 'vegasfat1')
 
-    subject = 'Request to remove profile'
-    body = """To whom it may concern,
-        I am an agent removing personal information on behalf of [Full name] and have been unsuccessful from removing data from your website. Per the information provided from your legal privacy policy, please remove the following page from your service: [url]
-        """
-    msg = f'Subject: {subject}\n\n{body}'
+        subject = 'Request to remove profile'
+        body = """To whom it may concern,
+            
+            I am an agent removing personal information on behalf of [Full name] and have been unsuccessful from removing data from your website. Per the information provided from your legal privacy policy, please remove the following page from your service: [url]
+            """
+        msg = f'Subject: {subject}\n\n{body}'
 
-    smtp.sendmail('cirrus1994@gmail.com', ['mokhtari@ucsb.edu', 'cirrus1994@gmail.com'], msg)
+        smtp.sendmail('cirrus1994@gmail.com', ['mokhtari@ucsb.edu', 'cirrus1994@gmail.com'], msg)
