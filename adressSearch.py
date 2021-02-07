aebd15cbefae2802b493046ff6509d4661dde878
cirrusm <cirrus1994@gmail.com>
2021-02-06 18:40:47 -0800

    email

--- adressSearch.py
+++ adressSearch.py
@@ -12,4 +12,10 @@ email = 'Cirrus1994@gmail.com'
 web = webdriver.Chrome()
 web.get('https://www.addresssearch.com/remove-info.php')
 
+# First last name
 web.find_element_by_xpath('//*[@id="body_container"]/div[1]/div/form/table/tbody/tr[1]/td[2]/input[1]').send_keys(firstname)
+web.find_element_by_xpath('//*[@id="body_container"]/div[1]/div/form/table/tbody/tr[1]/td[2]/input[2]').send_keys(lastname)
+
+# Email
+
+web.find_element_by_xpath('//*[@id="body_container"]/div[1]/div/form/table/tbody/tr[2]/td[2]/input').send_keys(email)
