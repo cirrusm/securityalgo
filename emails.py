919eab3e1e245479c4418d590a1017c1dcb35670
cirrusm <cirrus1994@gmail.com>
2021-02-06 20:52:35 -0800

    os

--- emails.py
+++ emails.py
@@ -1,3 +1,9 @@
+import os
 import smtplib
 
-with smtplib.SMTP('smtp.gmail.com', 587)
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login()
