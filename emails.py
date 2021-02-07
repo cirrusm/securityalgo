341123367d15a837eefb5b2265e7e0cd1d9458e4
cirrusm <cirrus1994@gmail.com>
2021-02-06 20:55:43 -0800

    subject

--- emails.py
+++ emails.py
@@ -7,3 +7,9 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
 
     smtp.login('cirrus1994', 'Poloman16')
+
+    subject = 'Request to remove profile'
+    body = """To whom it may concern,
+        I am an agent removing personal information on behalf of [Full name] and have been unsuccessful from removing data from your website. Per the information provided from your legal privacy policy, please remove the following page from your service: [url]
+        """
+    msg = f'Subject: {subject}\n\n{body}'
