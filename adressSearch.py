8444d3ddfc37adbc671fe0e5b0ed79555fd96474
cirrusm <cirrus1994@gmail.com>
2021-02-06 18:38:45 -0800

    firstname

--- adressSearch.py
+++ adressSearch.py
@@ -8,3 +8,8 @@ address = '4501 surrey drive'
 city = 'Corona del mar'
 zip_code = '92625'
 email = 'Cirrus1994@gmail.com'
+
+web = webdriver.Chrome()
+web.get('https://www.addresssearch.com/remove-info.php')
+
+web.find_element_by_xpath('//*[@id="body_container"]/div[1]/div/form/table/tbody/tr[1]/td[2]/input[1]').send_keys(firstname)
