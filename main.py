4d88cca3ee3ec57abcc75258885e310dd5f4bfad
cirrusm <cirrus1994@gmail.com>
2021-02-06 21:19:03 -0800

    COMMENTS'

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import addressSearch
 import lexisnexis
 
+## NEED TO GET:
+## FIRSTNAME
+## LASTNAME
+## ADDRESS
+## CITY
+
 addressSearch.address_Search('Cirrus', 'Mokhtari', 'address', 'city', 'zip', 'email', 'state')
 lexisnexis.lexis('Cirrus', 'Mokhtari', 'address', 'city', 'zip')
