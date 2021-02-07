0758541777ce8b240fe7f15f6edc0059188cd077
cirrusm <cirrus1994@gmail.com>
2021-02-06 20:56:34 -0800

    basic template

--- emails.py
+++ emails.py
@@ -13,3 +13,5 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         I am an agent removing personal information on behalf of [Full name] and have been unsuccessful from removing data from your website. Per the information provided from your legal privacy policy, please remove the following page from your service: [url]
         """
     msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail('cirrus1994@gmail.com', 'reciever@gmail.com', msg)
