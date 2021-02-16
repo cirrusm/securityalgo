4c3719771a73650b3c61377a8dd4cea5cafd8429
cirrusm <cirrus1994@gmail.com>
2021-02-15 20:30:18 -0800

    click

--- removalTools/archives.py
+++ removalTools/archives.py
@@ -9,3 +9,5 @@ web.get('https://www.archives.com/optout')
 time.sleep(1)
 
 web.find_element_by_xpath('//*[@id="OptoutFirstName"]').send_keys('hi')
+
+web.find_element_by_xpath('/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div[18]/div/input').click()
