037701f5ba5dd550ee9993a5ce1398e371f126f9
cirrusm <cirrus1994@gmail.com>
2021-02-15 20:39:06 -0800

    comments

--- removalTools/archives.py
+++ removalTools/archives.py
@@ -8,7 +8,12 @@ web.get('https://www.archives.com/optout')
 
 time.sleep(1)
 
+## Fill out first last adress city state zip and email
 web.find_element_by_xpath('//*[@id="OptoutFirstName"]').send_keys('hi')
 web.find_element_by_xpath('//*[@id="OptoutMiddleName"]').send_keys('hiiiiiiiii')
+
+
+## click "i am this person"
+
 
 web.find_element_by_xpath('/html/body/div[1]/div/div/div/div/div[1]/div/form/div[2]/div[18]/div/input').click()
